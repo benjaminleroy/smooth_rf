04daa87e49259a5ca1a6519a256573eb226275c5
benjaminleroy <benjaminleroyusa@gmail.com>
2019-03-05 15:53:26 -0500

    varying t first

--- bash_scripts/depth_run_moon_test.py
+++ bash_scripts/depth_run_moon_test.py
@@ -2,4 +2,7 @@
 
 cd ../main/
 
-ipython depth_run_class.py moon 10 oob c eb p standard ce rf tree 10000 1
+
+ipython depth_run_class.py moon 10 oob c eb p standard ce rf tree 10000 .01
+ipython depth_run_class.py moon 10 oob c eb p standard ce rf tree 10000 .1
+ipython depth_run_class.py moon 10 oob c eb p standard ce rf tree 10000 10
