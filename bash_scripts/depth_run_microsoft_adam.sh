4d0bf766fa7d16ba97d76e5b1e9497592c4a76da
benjaminleroy <benjaminleroyusa@gmail.com>
2019-03-09 10:53:12 -0500

    adam on microsoft - varying t

---
+++ bash_scripts/depth_run_microsoft_adam.sh
@@ -0,0 +1,5 @@
+cd main/
+
+ipython depth_run_adam.py microsoft 10 ob c eb p standard rf tree 10000 1
+ipython depth_run_adam.py microsoft 10 ob c eb p standard rf tree 10000 10
+ipython depth_run_adam.py microsoft 10 ob c eb p standard rf tree 10000 .1
