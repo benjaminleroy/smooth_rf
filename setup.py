80812cda38ee605c148940535fa43789db09cc4a
benjaminleroy <benjaminleroyusa@gmail.com>
2019-03-24 22:37:39 -0400

    torch in setup

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='smooth_rf',
       packages=['smooth_rf'],
       install_requires=[
           'numpy', 'sparse', 'scipy', 'sklearn',
+          'torch', # for pytorch approach
           'quadprog',  # used for smooth_level (remove in the future)
           'progressbar2', 'matplotlib' # this line is less "needed"
       ],
