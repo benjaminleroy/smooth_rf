466cc3e3e9d0d5519c67d8fe5a910e54bddc9341
benjaminleroy <benjaminleroyusa@gmail.com>
2019-06-11 09:22:19 -0700

    analysis block code

---
+++ bash_scripts/analysis_block.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+cd ../main/
+
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
+ipython analysis.py microsoft reg 10
