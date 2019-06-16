239026767e200acd7f474b4430cfc07b2267f390
Benjamin Leroy <bpleroy@hydra3.stat.cmu.edu>
2019-06-16 15:33:43 -0400

    secondary analysis block

---
+++ bash_scripts/analysis_block2.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+
+cd ../main/
+
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
+ipython analysis.py microsoft class 10
